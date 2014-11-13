6d75ea05481bb61833d94dbba8b79825bdef78b1
Adam Barnhard <admbarn@gmail.com>
2014-11-13 16:45:31 -0600

    added binary tree

---
+++ binary-tree.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, text):
+        self.data = text
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    root = None
+
+    def insert(self, text):
+        if not self.root:
+            self.root = Node(text)
+        else:
+            self.set_next_node_position(text)
+
+    def set_next_node_position(self, text, node=None):
+        if not node:
+            node = self.root
+        if node.data > text:
+            if not node.left:
+                node.left = Node(text)
+            else:
+                self.set_next_node_position(text, node.left)
+        else:
+            if not node.right:
+                node.right = Node(text)
+            else:
+                self.set_next_node_position(text, node.right)
+
+tree = BinaryTree()
+
+tree.insert('f')
+tree.insert('c')
+tree.insert('b')
+tree.insert('e')
+tree.insert('h')
+pass
